99f03c67659f35a25382ebac8672e44da9761fcd
aidancer55 <bluebonnets77@gmail.com>
2022-02-26 23:06:35 +0300

    Добавьте файлы проекта.

---
+++ Async/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactAsync(n);
+
+            Console.ReadKey();
+        }
+        static void Fact(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("Факториал числа {0}! равен {1}", n, fact);
+        }
+        static async void FactAsync(int n)
+        {
+            await Task.Run(() => Fact(n));
+        }
+    }
+}
